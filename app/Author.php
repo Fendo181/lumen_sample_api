fdc37c7f3e36847157b98bc4874d2cf5af8cae3c
endu <futoshi.endo@pepabo.com>
2018-02-08 11:59:09 +0900

    Create Author Model

---
+++ app/Author.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+// Model
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [ 'name', 'email', 'github', 'twitter', 'location', 'latest_article_published' ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
+
