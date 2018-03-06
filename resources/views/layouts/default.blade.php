82ea14a1d1d70c10c6ae5954f285b7c6a0aa82e1
endu <futoshi.endo@pepabo.com>
2018-03-06 12:13:14 +0900

    bootstrapが有効にされてなかったので、CDNでCSSを読み込むようにした。

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -4,13 +4,11 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>@yield('title')</title>
 </head>
 <body>
 <div class="container">
     @yield('content')
 </div>
-<!-- BootStrap  -->
-{{--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js" integrity="sha384-feJI7QwhOS+hwpX2zkaeJQjeiwlhOP+SdQDqhgvvo1DsjtiSQByFdThsxO669S2D" crossorigin="anonymous"></script>--}}
-{{-- コンテンツの表示 --}}
 </body>
